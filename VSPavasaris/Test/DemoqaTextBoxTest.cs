38db92ae3a9d6c797610c02e0133c4fa951a9efe
LAPTOP-6DELA406\Simas <blizard1986@gmail.com>
2022-04-28 21:39:37 +0300

    page

---
+++ VSPavasaris/Test/DemoqaTextBoxTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VSPavasaris.page;
+
+namespace VSPavasaris.Test
+{
+    class DemoqaTextBoxTest
+    {
+        
+        private static IWebDriver _driver;
+        [OneTimeSetUp]
+
+        public void OneTimeSetup()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Window.Maximize();
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _driver.Url = "https://testpages.herokuapp.com/styled/calculator";
+        }
+        [OneTimeTearDown]
+        public void OneTimeTearDown()
+        {
+            _driver.Quit();
+        }
+        [Test]
+        public void InsertTextToFullNameField()
+        {
+            string fullName = "Simas";
+            DemoqaTextBoxPage demoqaTextBoxPage = new DemoqaTextBoxPage(_driver);
+            demoqaTextBoxPage.NavigateToDefaultPage();
+            demoqaTextBoxPage.InsertTextToFullNameField(fullName);
+            demoqaTextBoxPage.ClickSubmitButton();
+
+            demoqaTextBoxPage.VerifyFullNameResult(fullName);
+        }
+    }
+}
---
+++ VSPavasaris/page/demoqaTextBoxPage.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSPavasaris.page
+{
+    class DemoqaTextBoxPage
+    {
+        private static IWebDriver _driver;
+
+        private static IWebElement _fullNameInput => _driver.FindElement(By.Id("username"));
+        private static IWebElement _submitButton => _driver.FindElement(By.Id("submit"));
+        private static IWebElement _nameResult => _driver.FindElement(By.Id("name"));
+        private static IWebElement _popup => _driver.FindElement(By.Id("close-fixedban"));
+        public DemoqaTextBoxPage (IWebDriver webDriver)
+        {
+            _driver = new ChromeDriver();
+        }
+        public void NavigateToDefaultPage()
+        {
+            _driver.Url = "www.demoqa.com/text-box";
+        }
+        public void ClosePopUp()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            wait.Until(d => _popup.Displayed);
+            _popup.Click();
+        }
+        public void InsertTextToFullNameField(string fullName)
+        {
+            _fullNameInput.Clear();
+            _fullNameInput.SendKeys(fullName);
+        }
+        public void ClickSubmitButton()
+        {
+            _submitButton.Click();
+        }
+
+        public void VerifyFullNameResult(string expectedResult)
+        {
+            Assert.AreEqual($"Name:{expectedResult}", _nameResult.Text, "Name is wrong!");
+        }
+    }
+}
